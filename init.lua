9ece28be2a89d8d0a17c9030a88928c5202cb3ef
bel-wadoud <abdelwadoud.belheraoui@proton.me>
2026-02-18 21:01:06 +0000

    add rust & docker lsp's

--- init.lua
+++ init.lua
@@ -6,16 +6,26 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 
+
+-- Completely silence LSP deprecation warnings and other annoying notifications
+vim.notify = function(msg, log_level, opts)
+  -- Only show real errors, hide warnings and info messages
+  if log_level == vim.log.levels.ERROR then
+    vim.api.nvim_echo({ { msg, "ErrorMsg" } }, true, {})
+  end
+end
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -39,8 +49,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 
 -- shifting numbers and symbols
-local nums = {'1','2','3','4','5','6','7','8','9','0'}
-local specials = {'!','@','#','$','%','^','&','*','(',')'}
+local nums = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }
+local specials = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')' }
 
 for i = 1, #nums do
   vim.keymap.set('i', nums[i], specials[i], opts)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,17 +1,27 @@
 return {
+  -- Mason: installs tools
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
     config = function()
-      require("mason").setup()
+      require("mason").setup({ ui = { border = "rounded" } })
     end,
   },
 
+  -- Mason + LSP
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
-      require("mason-lspconfig").setup({
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      mason_lspconfig.setup({
         ensure_installed = {
           "lua_ls",
           "pyright",
@@ -20,10 +30,88 @@ return {
           "html",
           "cssls",
           "jsonls",
+          "clangd",
+          "rust_analyzer",
+          "dockerls",
+          "yamlls",
+          "docker_compose_language_service",
+          "marksman",
+          "taplo",
         },
         automatic_installation = true,
+      })
+
+      -- Manual setup for each server
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      })
+
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--completion-style=detailed",
+          "--header-insertion=iwyu",
+          "--function-arg-placeholders",
+        },
+      })
+
+      lspconfig.pyright.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        settings = { completions = { completeFunctionCalls = true } },
+      })
+
+      lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.docker_compose_language_service.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.yamlls.setup({
+        capabilities = capabilities,
+        settings = {
+          yaml = {
+            keyOrdering = false,
+          },
+        },
+      })
+
+      lspconfig.taplo.setup({ capabilities = capabilities })
+      lspconfig.marksman.setup({ capabilities = capabilities })
+
+
+      lspconfig.bashls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+        settings = { json = { validate = { enable = true } } },
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = true,
+            check = {
+              command = "clippy",
+            },
+          },
+        },
       })
     end,
   },
 }
-
